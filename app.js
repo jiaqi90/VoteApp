68373ff670298e1adc630b85a47ca8882fcfc615
Jiaqi Qian <jiaqi95@hotmail.com>
2017-01-02 15:21:30 -0500

    add highcharts and socket.io

--- app.js
+++ app.js
@@ -1,53 +1,50 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+/**
+ * Module dependencies.
+ */
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+var express = require('express')
+  , http = require('http')
+  , path = require('path')
+  , socketio = require('socket.io');
 
-var app = express();
+var app = express()
+  , server = http.createServer(app)
+  , io = socketio.listen(server);
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hjs');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', index);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'hjs');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.configure('development', function(){
+  app.use(express.errorHandler());
 });
 
-app.get('/', routes.index);
-app.post('/vote/sms', routes.voteSMS);
-
-http.createServer(app).listen(app.get('port'), function(){
+server.listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
 
-module.exports = app;
+
+io.configure('production', function(){
+  io.enable('browser client etag');
+  io.set('log level', 1);
+});
+
+io.configure('development', function(){
+  io.set('log level', 1);
+});
+
+io.sockets.on('connection', function(socket) {
+    socket.on('event', function(event) {
+        socket.join(event);
+    });
+});
+
+require('./routes')(app, io);
